6214cde49bc92c038feb0992270a36b76ee53fb7
Kolkenn <leonel.ponce96@gmail.com>
2024-10-15 12:43:38 -0400

    Chapter 2 - Part 5: Create a player object and render it on the screen.

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
-    pygame.init()
+    pygame.init() # Initialize pygame.
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen) # Render the player on the screen.
+        pygame.display.flip() # Update the full display Surface to the screen
+        
+        
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
