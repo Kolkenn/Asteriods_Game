c95e894e34f140e1faccdedaf98d22198be40101
Kolkenn <leonel.ponce96@gmail.com>
2024-10-15 21:50:47 -0400

    Chapter 3 Module 1: Asteroid Class and Asteriod Field Class. Objects are also group by what methods they support.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from player import Player
 from constants import *
 
@@ -6,8 +8,18 @@ def main():
     pygame.init() # Initialize pygame.
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    delta_time = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    asteroid_field = AsteroidField()
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,14 +31,18 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        for obj in updatable:
+            obj.update(delta_time)
+        #player.update(delta_time)
         
         screen.fill("black")
-        player.draw(screen) # Render the player on the screen.
+        for obj in drawable:
+            obj.draw(screen)
+        #player.draw(screen)
         pygame.display.flip() # Update the full display Surface to the screen
         
         
-        dt = clock.tick(60) / 1000
+        delta_time = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
